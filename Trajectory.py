2c220fdab2bb580bef1b759e980e7cafa77faef1
Deltaphish <joakimanderlind@gmail.com>
2015-06-19 22:32:12 +0200

    Updated Trajectory v2

--- Trajectory.py
+++ Trajectory.py
@@ -1,24 +1,31 @@
-
+#importing lib
 import matplotlib.pyplot as plt
 from pylab import *
 from math import sin, cos, radians, floor
-
+#main
 def tra(v, a, s, m):
-    posix = [0]
-    posiy = [s]
+    #declerations
+    posix = [0]#posx array
+    posiy = [s]#posy array
+    #start position
     posx=0
-    posy  =  0.1 + s
+    posy  =  0.001 + s 
     angle = radians(a)
+    #velocites
     velx  = v * cos(angle) 
     vely  = v * sin(angle)
-    while posy > 0:
-          fd = (0.5 * float(m) * float((velx**2)) * 0.3 * 0.2)/8
+    # main loop
+    while posy > 0: # while projectile is over ground
+          # air resistance
+          fdx = (0.5 * float(m) * float((velx**2)) * 0.3 * 0.2)/8 
+          fdy = (0.5 * float(m) * float((vely**2)) * 0.3 * 0.2)/8
+          # adding velocity
           posx = posx + velx/8 
           posy = posy + vely/8
-          vely = vely - (9.8/8)
-          velx = velx - fd
-          """posx = math.floor(posx)
-          posy = math.floor(posy)"""
+          # adding gravity and air resistance
+          vely = vely - (9.8/8) - fdy
+          velx = velx - fdx
+          # adding current position to final array
           posix.append(posx)
           posiy.append(posy)
           if posy < 0:
@@ -30,15 +37,19 @@ def tra(v, a, s, m):
        posx = posx - num
     posiy.append(posy)
     posix.append(posx)
+    # Creating plot
     plt.plot(posix, posiy)
-    plt.axis([0,15,0,15])
+    plt.axis([0,50,0,50])
     plt.xlabel("Distance in Meters")
     plt.ylabel("Hight in Meters")
     plt.show()
+    # Printing result to Console
     print(posix)
     print(posiy)
+# Geting input
 v_input = int(input('input your start velocity: '))
 a_input = int(input('input your launch angle: '))
 s_input = float(input('input your start heigth: '))
-m_input = float(input('input the mass of the object in kg: '))    
+m_input = float(input('input the mass of the object in kg: '))
+#Calling trajectory method    
 tra(v_input,a_input,s_input,m_input)
